3e8c3c3f4259e7a4c898040c8f3b51cfea7e409d
WottTheScoz <68659651+WottTheScoz@users.noreply.github.com>
2024-11-24 17:18:55 -0500

    Step 3 WIP

---
+++ WeatherAPI/Assets/Scripts/PlayFab/DeviceLogin.cs
@@ -0,0 +1,23 @@
+using PlayFab.ClientModels;
+using PlayFab;
+
+public class DeviceLogin : ILogin
+{
+    string deviceId;
+
+    public DeviceLogin(string deviceId)
+    {
+        this.deviceId = deviceId;
+    }
+
+    public void Login(System.Action<LoginResult> onSuccess, System.Action<PlayFabError> onFailure)
+    {
+        var request = new LoginWithCustomIDRequest
+        {
+            CustomId = deviceId,
+            CreateAccount = true
+        };
+
+        PlayFabClientAPI.LoginWithCustomID(request, onSuccess, onFailure);
+    }
+}
