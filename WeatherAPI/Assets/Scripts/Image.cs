7319cdc34bcf8287b3aae7e26164c6220abd53ce
Bernard <bernardthebear19@gmail.com>
2024-11-24 01:39:02 -0500

    image parsing

--- WeatherAPI/Assets/Scripts/Image.cs
+++ WeatherAPI/Assets/Scripts/Image.cs
@@ -1,20 +1,31 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
 public class Image : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
+    private const string webImage = "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/2560px-Cat_August_2010-4.jpg";
 
-    private const string webImage =
-        "https://upload.wikimedia.org/wikipedia/commons/thumb/1/15/Cat_August_2010-4.jpg/2560px-Cat_August_2010-4.jpg";
-}
+    private Texture2D billboard;
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        StartCoroutine(DownloadImage(storeImage));
+
+    }
+
+    public IEnumerator DownloadImage(Action<Texture2D> callback) { 
+        // add check here to see if texture is taken
+        UnityWebRequest request = UnityWebRequestTexture.GetTexture(webImage);
+        yield return request.SendWebRequest();
+        callback(DownloadHandlerTexture.GetContent(request));
+
+    }
+
+    public void storeImage(Texture2D image)
+    {
+        image = billboard;
     }
 }
